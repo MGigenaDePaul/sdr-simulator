dba853afa1a3200cfbb4647c3b3298e4e3c64a46
Miqueas <proyectosmiqueas@gmail.com>
2026-04-14 09:51:41 -0300

    adding leadinfo and styling

--- src/pages/Simulation.jsx
+++ src/pages/Simulation.jsx
@@ -2,20 +2,45 @@ import { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/Simulation.css";
  
-const Simulation = () => {
-  const [selectedLed, setSelectedLead] = useState(null);
+const angryLeadInfo = {
+  mood: 'Angry',
+  name: 'Mariano',
+  manuscript: 'Uncertain',
+}
 
+const Simulation = () => {
+  const navigate = useNavigate();
+  const [selectedLead, setSelectedLead] = useState(null);
+  
   return (
     <div className='simulator'>
       <div className='sim-typeOfLead'>
-        <h2>Choose a type of lead</h2>
-        <div className='sim-lead-buttons'>
-          <button>angry</button>
-          <button>happy</button>
-          <button>busy</button>
+        
+        <div>
+          {!selectedLead && (
+            <div>
+              <h2>Choose a type of lead</h2>
+              <div className='sim-lead-buttons'>
+                <button onClick={() => setSelectedLead(angryLeadInfo)}>angry</button>
+                <button>happy</button>
+                <button>busy</button>
+              </div>
+            </div>
+          )}
+
+          {selectedLead && (
+            <div>
+              <div className='sim-lead-info'>
+                <h3>{selectedLead.mood} lead info</h3>
+                <p><b>Name:</b> {selectedLead.name}</p>
+                <p><b>Manuscript:</b> {selectedLead.manuscript}</p>
+                <button onClick={() => setSelectedLead(null)}>Choose another lead</button>
+              </div>
+            </div>
+          )}
         </div>
       </div>   
-      <button className='home-button'>Go home</button> 
+      <button onClick={() => navigate('/')} className='home-button'>Go home</button> 
     </div>
   )
 }
